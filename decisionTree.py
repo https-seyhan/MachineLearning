40d6a490266c2b0a92e722019e8d07c867ec12e2
Data Science | Machine Learning | Deep Learning | Text Analytics | Web Analytics |Python | SAS | R <49085331+SaulML@users.noreply.github.com>
2019-05-02 12:49:43 +1000

    Add files via upload
    This codes shows different between Linear Regression and Decision Tree regression in visual format.

---
+++ decisionTree.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Apr 29 14:59:05 2019
+
+@author: saul
+"""
+import numpy as np
+import mglearn
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
+from matplotlib import pyplot as plt
+
+
+X, y = mglearn.datasets.make_wave(n_samples=100)
+
+print(len(X))
+print(len(y))
+
+line = np.linspace(-3, 3, 1000, endpoint=False).reshape(-1,1)   
+
+reg = DecisionTreeRegressor(min_samples_split=3).fit(X,y)
+print(reg)
+plt.figure()
+plt.plot(line, reg.predict(line), label ="decision tree")
+
+reg = LinearRegression().fit(X,y)
+plt.plot(line, reg.predict(line), label="linear regression")
+
+plt.plot(X[:, 0], y, 'o', c='k')
+plt.ylabel("Regression output")
+plt.xlabel("Input feature")
+plt.legend(loc="best")
