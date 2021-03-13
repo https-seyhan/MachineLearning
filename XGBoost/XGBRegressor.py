a986ed72c4dcc6c548595a2af0431b591e48171c
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-03-13 11:45:39 +1100

    Update XGBRegressor.py

--- XGBoost/XGBRegressor.py
+++ XGBoost/XGBRegressor.py
@@ -51,7 +51,6 @@ X_train, X_test, y_train, y_test = train_test_split(trainx, trainy, test_size= 0
 
 # Paramater list in a dictionary
 param_dist = {'n_estimators':200, 'learning_rate':0.01, 'max_depth':7}
-
 clf = xgboost.XGBRegressor(**param_dist)
 clf.fit(X_train, y_train, eval_metric='logloss')
 
