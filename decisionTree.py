87bfae0bb422658f1adc321de552898fcb06ea5b
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-06-02 20:31:24 +1000

    Update decisionTree.py

--- decisionTree.py
+++ decisionTree.py
@@ -18,6 +18,7 @@ plt.figure()
 plt.plot(line, reg.predict(line), label ="decision tree")
 
 reg = LinearRegression().fit(X,y)
+
 plt.plot(line, reg.predict(line), label="linear regression")
 plt.plot(X[:, 0], y, 'o', c='k')
 plt.ylabel("Regression output")
