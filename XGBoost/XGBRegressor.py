428edb53ac8e994bc8fbbf66b80cd37f3ea82f27
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-08-14 09:56:42 +1000

    Update XGBRegressor.py

--- XGBoost/XGBRegressor.py
+++ XGBoost/XGBRegressor.py
@@ -5,7 +5,7 @@
 """
 import pandas as pd
 import numpy as np
-import xgboost
+import xgboost # XGBoost module
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder # Encode target labels with value between 0 and n_classes-1
