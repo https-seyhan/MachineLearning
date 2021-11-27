077f024a5ba837eca7d8b561e9f5bd83a08886f5
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-27 11:50:03 +1100

    Update XGBRegressor.py

--- XGBoost/XGBRegressor.py
+++ XGBoost/XGBRegressor.py
@@ -51,7 +51,7 @@ insurance_xg_regression.fit(X_train, y_train, eval_metric='logloss')
 
 #print("Model :", clf)
 #evals_result = clf.evals_result()
-y_testpred = clf.predict(X_test)
+y_testpred = insurance_xg_regression.predict(X_test)
 y_pred = clf.predict(test)
 
 dftestpred = pd.DataFrame(y_testpred)
