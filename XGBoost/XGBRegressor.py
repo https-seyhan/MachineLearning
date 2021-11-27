42cbffa6f7ef9ecb47a33b0c2f339b65b3553bbf
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2021-11-27 12:01:27 +1100

    Update XGBRegressor.py

--- XGBoost/XGBRegressor.py
+++ XGBoost/XGBRegressor.py
@@ -43,6 +43,8 @@ trainy = train['charges'].astype('int')
 trainx = train.drop(['charges'], axis=1)
 test = test.drop(['charges'], axis=1)
 
+
+#Cut training and Test Data
 X_train, X_test, y_train, y_test = train_test_split(trainx, trainy, test_size= 0.3)
 
 # Paramater list in a dictionary
