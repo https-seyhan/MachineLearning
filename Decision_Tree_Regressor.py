7536652b745959fbd444da2a0769f2339b8828fd
Artificial Intelligence | Machine Learning | Deep Learning | Text Analytics | Web Analytics | Tensorflow | Python | SAS | R <sauldata@tutanota.com>
2022-04-11 18:07:03 +1000

    Update Decision_Tree_Regressor.py

--- Decision_Tree_Regressor.py
+++ Decision_Tree_Regressor.py
@@ -64,7 +64,6 @@ plt.xlabel('Production Cost')
 plt.ylabel('Profit') 
 # show the plot 
 plt.show() 
-
 #generate decision tree visualization
 dotfile = export_graphviz (reg, out_file = None, feature_names = ['Production Cost'])
 graph = graphviz.Source(dotfile)
